4f7965c5318aed0c492a7ff6dbdf98e3a5e61fff
Vaidotas Valuckas <vaidotas.valuckas@gmail.com>
2023-02-11 04:58:24 +0200

    Extract MySql specifics

--- lib/src/main/java/io/github/rieske/dbtest/extension/mysql/MysqlTestExtension.java
+++ lib/src/main/java/io/github/rieske/dbtest/extension/mysql/MysqlTestExtension.java
@@ -1,28 +1,14 @@
 package io.github.rieske.dbtest.extension.mysql;
 
-import com.mysql.cj.jdbc.MysqlDataSource;
 import io.github.rieske.dbtest.DatabaseTestExtension;
 import org.junit.jupiter.api.extension.AfterEachCallback;
 import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
-import org.testcontainers.containers.MySQLContainer;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.util.Map;
 
 public abstract class MysqlTestExtension extends DatabaseTestExtension implements BeforeEachCallback, AfterEachCallback {
-    protected static final MySQLContainer<?> DB_CONTAINER =
-            new MySQLContainer<>("mysql:8.0.32").withReuse(true);
-    private static final String JDBC_URI;
-
-    static {
-        DB_CONTAINER.withTmpFs(Map.of("/var/lib/mysql", "rw"));
-        DB_CONTAINER.withCommand("mysqld", "--innodb_flush_method=nosync");
-        DB_CONTAINER.start();
-        JDBC_URI = "jdbc:mysql://" + DB_CONTAINER.getHost() + ":" + DB_CONTAINER.getMappedPort(3306);
-    }
+    protected static final MysqlTestDatabase database = new MysqlTestDatabase();
 
     @Override
     public void beforeEach(ExtensionContext context) {
@@ -31,28 +17,11 @@ public abstract class MysqlTestExtension extends DatabaseTestExtension implement
 
     @Override
     public void afterEach(ExtensionContext context) {
-        executeInDefaultDatabase("DROP DATABASE " + databaseName);
+        database.executeInDefaultDatabase("DROP DATABASE " + databaseName);
     }
 
     @Override
     public DataSource getDataSource() {
-        return dataSourceForDatabase(databaseName);
-    }
-
-    protected static DataSource dataSourceForDatabase(String databaseName) {
-        var dataSource = new MysqlDataSource();
-        dataSource.setUrl(JDBC_URI + "/" + databaseName);
-        dataSource.setUser("root");
-        dataSource.setPassword(DB_CONTAINER.getPassword());
-        return dataSource;
-    }
-
-    protected void executeInDefaultDatabase(String sql) {
-        var dataSource = dataSourceForDatabase(DB_CONTAINER.getDatabaseName());
-        try (Connection conn = dataSource.getConnection()) {
-            conn.createStatement().execute(sql);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        return database.dataSourceForDatabase(databaseName);
     }
 }
--- lib/src/test/java/io/github/rieske/dbtest/extension/mysql/MysqlFastTestExtension.java
+++ lib/src/test/java/io/github/rieske/dbtest/extension/mysql/MysqlFastTestExtension.java
@@ -1,9 +1,6 @@
 package io.github.rieske.dbtest.extension.mysql;
 
 import org.flywaydb.core.Flyway;
-import org.testcontainers.containers.Container;
-
-import java.io.IOException;
 
 public class MysqlFastTestExtension extends MysqlTestExtension {
 
@@ -11,39 +8,15 @@ public class MysqlFastTestExtension extends MysqlTestExtension {
 
     static {
         Flyway.configure()
-                .dataSource(dataSourceForDatabase(DB_CONTAINER.getDatabaseName()))
+                .dataSource(database.dataSourceForDatabase(database.getMasterDatabaseName()))
                 .load()
                 .migrate();
-        dumpDatabase();
-    }
-
-    private static Container.ExecResult runInDatabaseContainer(String command) {
-        try {
-            return DB_CONTAINER.execInContainer("bash", "-c", command);
-        } catch (IOException | InterruptedException e) {
-            throw new RuntimeException("Error running command in database container: " + command, e);
-        }
-    }
-
-    private static void dumpDatabase() {
-        String command = "mysqldump -u root --password=%s %s > %s".formatted(DB_CONTAINER.getPassword(), DB_CONTAINER.getDatabaseName(), DB_DUMP_FILENAME);
-        Container.ExecResult result = runInDatabaseContainer(command);
-        if (result.getExitCode() != 0) {
-            throw new RuntimeException("Error dumping database: " + result);
-        }
-    }
-
-    private void restoreDatabase() {
-        String command = "mysql -u root --password=%s %s < %s".formatted(DB_CONTAINER.getPassword(), databaseName, DB_DUMP_FILENAME);
-        Container.ExecResult result = runInDatabaseContainer(command);
-        if (result.getExitCode() != 0) {
-            throw new RuntimeException("Error restoring database: " + result);
-        }
+        database.dumpDatabase(DB_DUMP_FILENAME);
     }
 
     @Override
     protected void createFreshMigratedDatabase() {
-        executeInDefaultDatabase("CREATE DATABASE " + databaseName);
-        restoreDatabase();
+        database.executeInDefaultDatabase("CREATE DATABASE " + databaseName);
+        database.restoreDatabase(databaseName, DB_DUMP_FILENAME);
     }
 }

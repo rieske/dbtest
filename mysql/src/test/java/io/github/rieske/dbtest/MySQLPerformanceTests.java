707b27f7e645e72e984b10394b6a6aebb543f3cf
Vaidotas Valuckas <vaidotas.valuckas@gmail.com>
2023-02-14 08:16:29 +0200

    Add databae per test class tests (disabled for now)

--- mysql/src/test/java/io/github/rieske/dbtest/MySQLPerformanceTests.java
+++ mysql/src/test/java/io/github/rieske/dbtest/MySQLPerformanceTests.java
@@ -4,29 +4,29 @@ import io.github.rieske.dbtest.extension.mysql.FlywayMySQLFastTestExtension;
 import io.github.rieske.dbtest.extension.mysql.FlywayMySQLSlowTestExtension;
 import org.junit.jupiter.api.Nested;
 
-class MySQLTests {
-    abstract static class PostgresSlowAndFastTests extends SlowAndFastTests {
-        PostgresSlowAndFastTests(String version) {
+class MySQLPerformanceTests {
+    abstract static class TestTemplate extends PerformanceTests {
+        TestTemplate(String version) {
             super(version, FlywayMySQLSlowTestExtension::new, FlywayMySQLFastTestExtension::new);
         }
     }
 
     @Nested
-    class Mysql8Tests extends PostgresSlowAndFastTests {
+    class Mysql8Tests extends TestTemplate {
         Mysql8Tests() {
             super("8.0.32");
         }
     }
 
     @Nested
-    class Mysql8031Tests extends PostgresSlowAndFastTests {
+    class Mysql8031Tests extends TestTemplate {
         Mysql8031Tests() {
             super("8.0.31");
         }
     }
 
     @Nested
-    class Mysql8030Tests extends PostgresSlowAndFastTests {
+    class Mysql8030Tests extends TestTemplate {
         Mysql8030Tests() {
             super("8.0.30");
         }
